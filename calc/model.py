8b71eede0ee7b9ad65ad983865a7d0a94d0f6a02
Eugene Menski <eugene.menski@gmail.com>
2022-07-29 23:18:03 +0300

    Added model file

--- calc/model.py
+++ calc/model.py
@@ -1,12 +1,19 @@
-x = 0
-y = 0
-
-def init(a, b):
-    global x
-    global y
-    x = a
-    y = b
+from email.policy import default
+from statistics import mode
 
 
-def sum():
-    return x + y
+def calculate(data, mode_key = 0):
+    match mode_key:
+        case 0:
+            return calc_real_numbers(data)
+        case 1:
+            return "Rational"
+        case 2:
+            return "Imaginary"
+        case default:
+            return "No mode"
+
+def calc_real_numbers(data):
+    result = eval(data)
+    result = str(round(float(result), 3))
+    return result
