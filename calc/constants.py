a8a08c3492aa286249c854d5a3ab6048f03521d2
Eugene Menski <eugene.menski@gmail.com>
2022-07-29 23:15:41 +0300

    Added constants file

---
+++ calc/constants.py
@@ -0,0 +1,11 @@
+calc_modes = ['Вещественные числа', 'Рациональные числа', 'Комплексные числа']
+calc_mode_label = 'Режим работы'
+calc_mode_default = 0
+app_name = 'Калькулятор'
+empty_string = ''
+combobox_key = 'mode'
+clear_button_text = 'c'
+remove_button_text = '«'
+max_input_length = 25
+calculate_button_text = '='
+quit_button_text = 'Quit'
