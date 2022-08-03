477029e9d8b6b47b76263f923129cc3049dc78bd
Eugene Menski <eugene.menski@gmail.com>
2022-08-03 13:09:55 +0300

    Added show salaries option

--- organization/reader.py
+++ organization/reader.py
@@ -1,3 +1,5 @@
+dept_header = "   ID   |       Created        |        Updated        |     Name     "
+
 def read_departments(path):
     data = open(path, 'r')
     for line in data:
@@ -9,5 +11,18 @@ def read_departments(path):
 def show_items(file_name):
     path = 'organization/' + file_name + '.csv'
     if file_name == "departments":
-        print("   ID   |       Created        |        Updated        |     Name     ")
+        print(dept_header)
         read_departments(path)
+    if file_name == "salaries":
+        print(salaries_header)
+        read_salaries(path)
+
+salaries_header = "   ID   |       Created        |        Updated        |  Amount  "
+
+def read_salaries(path):
+    data = open(path, 'r')
+    for line in data:
+        line = line.replace("\n", "")
+        list_items = line.split(';')
+        print(f"  {list_items[0]} | {list_items[2]}  | {list_items[3]}   |  {list_items[1]}  ")
+    data.close()
