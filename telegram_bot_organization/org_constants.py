eb8acc0c03151efa511aa6efe1ad5e817c9c4ab2
Eugene Menski <eugene.menski@gmail.com>
2022-08-11 16:57:28 +0300

    removed unnecessary files

--- telegram_bot_organization/org_constants.py
+++
@@ -1,7 +0,0 @@
-
-main_menu_items = ["", "employees", "salaries", "exit"]
-
-choose_action_text = "Выберите действие:"
-sub_menu_items = ["Список", "Добавить", "Изменить", "Удалить", "Вернуться в главное меню"]
-
-open_header = "База данных организации"
